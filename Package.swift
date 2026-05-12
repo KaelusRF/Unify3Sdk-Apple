863b3b1c382b4012bea8fefc45cb5011ebc78094
Ben Murray <ben.murray@kaelus.com>
2026-05-12 15:56:54 +1000

    Release 0.1.1

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Unify3Sdk",
-            url: "https://github.com/KaelusRF/Unify3Sdk-Apple/releases/download/0.1.0/Unify3Sdk-0.1.0.xcframework.zip",
-            checksum: "1fa820dc6f205c84b6955b56c02a85460906e2abd5927393a5d3007745623d26"
+            url: "https://github.com/KaelusRF/Unify3Sdk-Apple/releases/download/0.1.1/Unify3Sdk-0.1.1.xcframework.zip",
+            checksum: "368992eb91e1ee8618d5d9a1d40c579f80ea40eb2ff300da757e22264847dddf"
         ),
         .binaryTarget(
             name: "Unify3Core",
-            url: "https://github.com/KaelusRF/Unify3Sdk-Apple/releases/download/0.1.0/Unify3Core-0.1.0.xcframework.zip",
-            checksum: "def9a7eaafaf7e475847476c66d05e7a33625ca1331cb8d289b03ed4391cce80"
+            url: "https://github.com/KaelusRF/Unify3Sdk-Apple/releases/download/0.1.1/Unify3Core-0.1.1.xcframework.zip",
+            checksum: "89989ff2da01ba4fb07f77a7bd306da732c29a88d71d7b74e178647182e53728"
         )
     ]
 )
